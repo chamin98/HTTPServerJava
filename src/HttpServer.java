562c7d6cf08cdd1aea5c4b05e6deb62f785f8d4b
cs_tg <61661109+chamin98@users.noreply.github.com>
2024-07-29 13:01:04 +0530

    first step

---
+++ src/HttpServer.java
@@ -0,0 +1,11 @@
+import java.net.ServerSocket;
+
+public class HttpServer{
+    public static void main(String[] args) throws Exception {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listning for connection on port 8080");
+        while(true){
+            
+        }
+    }
+}
