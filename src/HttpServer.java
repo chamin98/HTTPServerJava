21378c1552bded5d1905f8d6df59b70e579e3eee
cs_tg <61661109+chamin98@users.noreply.github.com>
2024-07-29 14:44:12 +0530

    final

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class HttpServer{
     public static void main(String[] args) throws Exception {
@@ -9,14 +10,20 @@ public class HttpServer{
         System.out.println("Listning for connection on port 8080");
         while(true){
             // final Socket client = server.accept();
-            Socket clientSocket = server.accept();
-            InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
+            // Socket clientSocket = server.accept();
+            // InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            // BufferedReader reader = new BufferedReader(isr);
+            // String line = reader.readLine();
 
-            while (!line.isEmpty()) {
-                System.out.println(line);
-                line = reader.readLine();
+            // while (!line.isEmpty()) {
+            //     System.out.println(line);
+            //     line = reader.readLine();
+            // }
+
+            try (Socket socket = server.accept()) {
+                Date today = new Date();
+                String httpResponse = "HTTP/1.1 200 OK" + today;
+                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
         }
     }
