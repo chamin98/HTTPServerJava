edff3fd2e0dacb4ef7241ff6034a75ae89f231a2
cs_tg <61661109+chamin98@users.noreply.github.com>
2024-07-29 13:22:01 +0530

    standard HTTP Server

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -1,11 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HttpServer{
     public static void main(String[] args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listning for connection on port 8080");
         while(true){
-            
+            final Socket client = server.accept();
+            // Socket clientSocket = server.accept();
+            // InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+            // BufferedReader reader = new BufferedReader(isr);
+            // String line = reader.readLine();
+
+            // while (!line.isEmpty()) {
+            //     System.out.println(line);
+            //     line = reader.readLine();
+            // }
         }
     }
 }
